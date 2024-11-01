bc2c02d14140c5ddce003cc4964f80194ad6e9da
Luciano Vasconcelos <lvgalvaofilho@gmail.com>
2024-10-31 21:37:01 -0300

    funcionalidade2

--- src/extract.py
+++ src/extract.py
@@ -17,9 +17,6 @@ def extracao_de_arquivos_excel(path_dos_arquivos: str) -> list[DataFrame]:
 
     all_files: list[Path] = list(Path(path_dos_arquivos).glob("*.xlsx"))
 
-    if len(all_files) == 0:
-        raise ValueError("No Excel files found")
-
     list_dfs = []
 
     for files in all_files:
